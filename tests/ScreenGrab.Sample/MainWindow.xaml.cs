8968429842918fdaa807afc57a4f81d4f3b03bc8
zggsong <zggsong@foxmail.com>
2024-07-29 18:16:29 +0800

    chore: 更新调用

--- tests/ScreenGrab.Sample/MainWindow.xaml.cs
+++ tests/ScreenGrab.Sample/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using Dapplo.Windows.User32;
+using ScreenGrab.Extensions;
 using ScreenGrab.Utilities;
 
 namespace ScreenGrab.Sample;
@@ -16,11 +18,58 @@ public partial class MainWindow : Window
     private void Capture_Click(object sender, RoutedEventArgs e)
     {
         Clean();
-        var view = new Grab();
-        if (view.ShowDialog() == true)
+
+        NewScreenGrab();
+
+        // var view = new Grab();
+        // if (view.ShowDialog() == true)
+        // {
+        //     var result = view.Image;
+        //     Img.Source = ImageMethods.BitmapToImageSource(result);
+        // }
+    }
+
+    private void NewScreenGrab()
+    {
+        DisplayInfo[] allScreens = DisplayInfo.AllDisplayInfos;
+        WindowCollection allWindows = Application.Current.Windows;
+
+        List<Grab> allFullscreenGrab = new();
+
+        int numberOfScreens = allScreens.Count();
+
+        foreach (Window window in allWindows)
+            if (window is Grab grab)
+                allFullscreenGrab.Add(grab);
+
+        int numberOfFullscreenGrabWindowsToCreate = numberOfScreens - allFullscreenGrab.Count;
+
+        for (int i = 0; i < numberOfFullscreenGrabWindowsToCreate; i++)
         {
-            var result = view.Image;
-            Img.Source = ImageMethods.BitmapToImageSource(result);
+            allFullscreenGrab.Add(new Grab());
+        }
+
+        int count = 0;
+
+        double sideLength = 40;
+
+        foreach (DisplayInfo screen in allScreens)
+        {
+            Grab fullScreenGrab = allFullscreenGrab[count];
+            fullScreenGrab.WindowStartupLocation = WindowStartupLocation.Manual;
+            fullScreenGrab.Width = sideLength;
+            fullScreenGrab.Height = sideLength;
+            fullScreenGrab.WindowState = WindowState.Normal;
+
+            Point screenCenterPoint = screen.ScaledCenterPoint();
+
+            fullScreenGrab.Left = screenCenterPoint.X - (sideLength / 2);
+            fullScreenGrab.Top = screenCenterPoint.Y - (sideLength / 2);
+
+            fullScreenGrab.Show();
+            fullScreenGrab.Activate();
+
+            count++;
         }
     }
 
